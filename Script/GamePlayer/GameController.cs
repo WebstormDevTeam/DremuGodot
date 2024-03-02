e11835ac986725490b84b7b3c4afbcab21c97def
webstorm-dxy <dxy20070515@163.com>
2024-03-02 23:12:44 +0800

    添加了LineRenderer的创建函数

--- Script/GamePlayer/GameController.cs
+++ Script/GamePlayer/GameController.cs
@@ -1,12 +1,19 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using Godot.Collections;
 
 namespace DremuGodot.Script.GamePlayer
 {
 	public partial class GameController : Node
 	{
-
-		
+		public override void _Ready()
+		{
+			string chartPath = "res://Chart/TestJson.json";
+			string jsonString = FileAccess.GetFileAsString(chartPath);
+			Dictionary json = Json.ParseString(jsonString).AsGodotDictionary();
+			
+			GD.Print(json["Name"]);
+		}
 	}
 }
