6d2b4805b68cd96aeea0f0ceaa57545ebd397aa3
webstorm-dxy <dxy20070515@163.com>
2024-03-03 12:30:03 +0800

    添加JSON解析的代码（使用Newtonsoft）

---
+++ Script/GamePlayer/ChartAnalyser.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DremuGodot.Script.GamePlayer;
+
+public class ChartAnalyser
+{
+    /// <summary>
+    /// 获取谱面数据
+    /// </summary>
+    /// <param name="json">传入一个字符串，字符串是JSON格式的谱面文件</param>
+    /// <returns>谱面数据，c＃对象格式</returns>
+    public static Root GetChartData(string json)
+    {
+        string jsonString = json;
+        Root obj = JsonConvert.DeserializeObject<Root>(jsonString);
+        return obj;            
+    }
+}
+
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+public class Action
+{
+    public string ActionName;
+    public List<List<int>> Time;
+    public List<int> Rot;
+    public List<List<int>> Pos;
+}
+
+public class Chart
+{
+    public List<CoordinateSystem> CoordinateSystems;
+}
+
+public class CoordinateSystem
+{
+    public string Num;
+    public List<int> Pos;
+    public int Rot;
+    public List<SubCoordinateSystem> SubCoordinateSystems;
+    public List<GuideLines> GuideLines;
+    public List<Action> Actions;
+}
+
+public class GuideLines
+{
+    public int Num;
+    public bool JudgementPointIsShow;
+    public List<Line> lines;
+}
+
+public class Line
+{
+    public int Num;
+    public List<int> Time;
+    public string LineType;
+    public List<int> Start;
+    public List<int> End;
+    public List<int> p1;
+    public List<int> p2;
+}
+
+public class Root
+{
+    public string Name;
+    public int Version;
+    public string Hard;
+    public int bpm;
+    public Chart Chart;
+}
+
+public class SubCoordinateSystem
+{
+    public string Num;
+    public List<int> Pos;
+    public int Rot;
+    public List<object> SubCoordinateSystems;
+    public List<GuideLines> GuideLines;
+    public List<Action> Actions;
+}
+
--- Script/GamePlayer/GameController.cs
+++ Script/GamePlayer/GameController.cs
@@ -2,6 +2,40 @@ using Godot;
 using System;
 using System.Collections.Generic;
 using Godot.Collections;
+using Newtonsoft.Json;
+
+/*                            _ooOoo_
+ *                           o8888888o
+ *                           88" . "88
+ *                           (| -_- |)
+ *                            O\ = /O
+ *                        ____/`---'\____
+ *                      .   ' \\| |// `.
+ *                       / \\||| : |||// \
+ *                     / _||||| -:- |||||- \
+ *                       | | \\\ - /// | |
+ *                     | \_| ''\---/'' | |
+ *                      \ .-\__ `-` ___/-. /
+ *                   ___`. .' /--.--\ `. . __
+ *                ."" '< `.___\_<|>_/___.' >'"".
+ *               | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ *                 \ \ `-. \_ __\ /__ _/ .-` / /
+ *         ======`-.____`-.___\_____/___.-`____.-'======
+ *                            `=---='
+ *
+ *         .............................................
+ *                  佛祖镇楼                  BUG辟易
+ *          佛曰:
+ *                  写字楼里写字间，写字间里程序员；
+ *                  程序人员写程序，又拿程序换酒钱。
+ *                  酒醒只在网上坐，酒醉还来网下眠；
+ *                  酒醉酒醒日复日，网上网下年复年。
+ *                  但愿老死电脑间，不愿鞠躬老板前；
+ *                  奔驰宝马贵者趣，公交自行程序员。
+ *                  别人笑我忒疯癫，我笑自己命太贱；
+ *                  不见满街漂亮妹，哪个归得程序员？
+ */
+
 
 namespace DremuGodot.Script.GamePlayer
 {
@@ -11,9 +45,9 @@ namespace DremuGodot.Script.GamePlayer
 		{
 			string chartPath = "res://Chart/TestJson.json";
 			string jsonString = FileAccess.GetFileAsString(chartPath);
-			Dictionary json = Json.ParseString(jsonString).AsGodotDictionary();
+			Root ChartData = ChartAnalyser.GetChartData(jsonString);
 			
-			GD.Print(json["Name"]);
+			GD.Print(ChartData.Chart.CoordinateSystems[0].SubCoordinateSystems);
 		}
 	}
 }
