57ee0e090b748784413c089ba6210454e5adce04
webstorm-dxy <dxy20070515@163.com>
2024-03-06 23:26:24 +0800

    将.net版本改成8.0，不要使用6.0，没法导出Android包

--- Script/GamePlayer/GuideLine/LineRenderer.cs
+++ Script/GamePlayer/GuideLine/LineRenderer.cs
@@ -7,96 +7,96 @@ namespace DremuGodot.Script.GamePlayer.GuideLine;
 
 public partial class LineRenderer : Node
 {
-    private Vector3[] _points;
-    public Queue<Vector2> PointsQueue = new Queue<Vector2>();
-    public int pointNumber = 100;
-    public static double speed;
-    private double DefaultSpeed = -2;
-    private int n = 0;
+	private Vector3[] _points;
+	public Queue<Vector2> PointsQueue = new Queue<Vector2>();
+	public int pointNumber = 100;
+	public static double speed;
+	private double DefaultSpeed = -2;
+	private int n = 0;
 
-    // 创建 Line2D 节点
-    Line2D line = new Line2D();
-    private Transform2D tran = new Transform2D();
+	// 创建 Line2D 节点
+	Line2D line = new Line2D();
+	private Transform2D tran = new Transform2D();
 
-    /// <summary>
-    /// 创建引导线（由多个三阶贝赛尔曲线构成）
-    /// </summary>
-    /// <param name="Curves">由两个List嵌套构成一个是曲线的List，一个是曲线的四个控制点</param>
-    public void CreateGuideLine(List<List<Vector2>> Curves)
-    {
-        foreach (List<Vector2> curve in Curves)
-        {
-            AddPoints(curve[0], curve[1], curve[2], curve[3]);
-        }
-        
-    }
-
-
-    /// <summary>
-    /// 将点加载到队列中，只可以调用一次，以节省内存空间
-    /// 使用贝赛尔曲线的方式进行绘制
-    /// </summary>
-    /// <param name="p0">起始端点</param>
-    /// <param name="p1">控制点1</param>
-    /// <param name="p2">控制点2</param>
-    /// <param name="p3">结束端点</param>
-    public void AddPoints(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3)
-    {
-        // GD.Print(line2);
-        // 循环生成点，并添加到队列中
-        for (float i = 0; i <= 1; i += 0.01f)
-        {
-            Vector2 point = Bezier.BezierCurve(p0, p1, p2, p3, i);
-            // point.Y = -point.Y;
-            PointsQueue.Enqueue(point);
-        }
-    }
-
-    /// <summary>
-    /// 将线渲染出来
-    /// </summary>
-    /// <param name="renderPointsNumber"></param>
-    public void RenderPoints()
-    {
-        //当计数大于渲染点数时，取点数；小于时，取当前计数
-        List<Vector2> points = PointsQueue.ToList()
-            .GetRange(0, PointsQueue.Count >= pointNumber ? pointNumber : PointsQueue.Count);
-        for (int i = 0; i < (PointsQueue.Count >= pointNumber ? pointNumber : PointsQueue.Count); i++)
-        {
-            float y = points[i].Y;
-            //n*delta以调整下落速度
-            points[i] = new Vector2(points[i].X, y - (float)(n * (speed > 0 && speed != null ? speed : DefaultSpeed)));
-            //TODO:当y==0时，将坐标传出到判定点上
-            if (points[i].Y > 0)
-            {
-                PointsQueue.Dequeue();
-            }
-        }
-
-        line.Points = points.ToArray();
-        n++;
-    }
+	/// <summary>
+	/// 创建引导线（由多个三阶贝赛尔曲线构成）
+	/// </summary>
+	/// <param name="Curves">由两个List嵌套构成一个是曲线的List，一个是曲线的四个控制点</param>
+	public void CreateGuideLine(List<List<Vector2>> Curves)
+	{
+		foreach (List<Vector2> curve in Curves)
+		{
+			AddPoints(curve[0], curve[1], curve[2], curve[3]);
+		}
+		
+	}
 
 
-    public override void _Ready()
-    {
-        // 设置折线的属性
-        line.Width = 10f; // 宽度为 10 像素
-        line.DefaultColor = new Color(1f, 0f, 0f); // 颜色为红色
-        line.TextureMode = Line2D.LineTextureMode.Tile; // 纹理模式为平铺
-        // line.Texture = GD.Load<Texture>("res://line_texture.png"); // 加载纹理资源
-        line.Points = new Vector2[100];
+	/// <summary>
+	/// 将点加载到队列中，只可以调用一次，以节省内存空间
+	/// 使用贝赛尔曲线的方式进行绘制
+	/// </summary>
+	/// <param name="p0">起始端点</param>
+	/// <param name="p1">控制点1</param>
+	/// <param name="p2">控制点2</param>
+	/// <param name="p3">结束端点</param>
+	public void AddPoints(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3)
+	{
+		// GD.Print(line2);
+		// 循环生成点，并添加到队列中
+		for (float i = 0; i <= 1; i += 0.01f)
+		{
+			Vector2 point = Bezier.BezierCurve(p0, p1, p2, p3, i);
+			// point.Y = -point.Y;
+			PointsQueue.Enqueue(point);
+		}
+	}
 
-        
-        /*AddPoints(new Vector2(0, 0), new Vector2(100, -1000), new Vector2(1000, -10), new Vector2(1000, -1000));*/
+	/// <summary>
+	/// 将线渲染出来
+	/// </summary>
+	/// <param name="renderPointsNumber"></param>
+	public void RenderPoints()
+	{
+		//当计数大于渲染点数时，取点数；小于时，取当前计数
+		List<Vector2> points = PointsQueue.ToList()
+			.GetRange(0, PointsQueue.Count >= pointNumber ? pointNumber : PointsQueue.Count);
+		for (int i = 0; i < (PointsQueue.Count >= pointNumber ? pointNumber : PointsQueue.Count); i++)
+		{
+			float y = points[i].Y;
+			//n*delta以调整下落速度
+			points[i] = new Vector2(points[i].X, y - (float)(n * (speed > 0 && speed != null ? speed : DefaultSpeed)));
+			//TODO:当y==0时，将坐标传出到判定点上
+			if (points[i].Y > 0)
+			{
+				PointsQueue.Dequeue();
+			}
+		}
 
-        // 将 Line2D 节点添加到场景中
-        AddChild(line);
-    }
+		line.Points = points.ToArray();
+		n++;
+	}
 
-    // 在 _Process 函数中更新折线的点
-    public override void _Process(double delta)
-    {
-        RenderPoints();
-    }
+
+	public override void _Ready()
+	{
+		// 设置折线的属性
+		line.Width = 10f; // 宽度为 10 像素
+		line.DefaultColor = new Color(1f, 0f, 0f); // 颜色为红色
+		line.TextureMode = Line2D.LineTextureMode.Tile; // 纹理模式为平铺
+		// line.Texture = GD.Load<Texture>("res://line_texture.png"); // 加载纹理资源
+		line.Points = new Vector2[100];
+
+		
+		/*AddPoints(new Vector2(0, 0), new Vector2(100, -1000), new Vector2(1000, -10), new Vector2(1000, -1000));*/
+
+		// 将 Line2D 节点添加到场景中
+		AddChild(line);
+	}
+
+	// 在 _Process 函数中更新折线的点
+	public override void _Process(double delta)
+	{
+		RenderPoints();
+	}
 }
