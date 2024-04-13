ab58f1c34fc24d384bf337481c693e58ef321c46
webstorm-dxy <dxy20070515@163.com>
2024-04-13 22:28:30 +0800

    有Bug，正在重写

--- Script/GamePlayer/NoteControllers/TapController.cs
+++ Script/GamePlayer/NoteControllers/TapController.cs
@@ -46,9 +46,9 @@ public partial class TapController : Node2D, INote
 		GD.Print("Touch");
 	}
 	// Called when the node enters the scene tree for the first time.
-	// public override void _Ready()
-	// {
-	// }
+	public override void _Ready()
+	{
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override async void _Process(double delta)
---
+++ Script/UniLib/Motion.cs
@@ -0,0 +1,6 @@
+namespace DremuGodot.Script.UniLib;
+
+public class Motion
+{
+    
+}
