4efc827ab7cfae19b87f48001a120eea55eed77a
Ice-Soul <18600213269@163.com>
2024-05-04 23:38:52 +0800

    添加了插件（？），并且修改了文件操作

--- Script/GamePlayer/GameController.cs
+++ Script/GamePlayer/GameController.cs
@@ -9,170 +9,170 @@ using Transform2D = Godot.Transform2D;
 
 namespace DremuGodot.Script.GamePlayer
 {
-    // 游戏控制器类
-    public partial class GameController : Node2D
-    {
-        // 导出场景属性
-        [Export] public PackedScene lineRenderer;
-        [Export] public PackedScene tap;
-        [Export] public PackedScene drag;
-        [Export] public PackedScene flick;
+	// 游戏控制器类
+	public partial class GameController : Node2D
+	{
+		// 导出场景属性
+		[Export] public PackedScene lineRenderer;
+		[Export] public PackedScene tap;
+		[Export] public PackedScene drag;
+		[Export] public PackedScene flick;
 
-        [Export] public Label NameLabel;
+		[Export] public Label NameLabel;
 
-        // 是否自动播放属性
-        [Export] public bool isAutoPlay;
+		// 是否自动播放属性
+		[Export] public bool isAutoPlay;
 
-        // 静态时间码和实例
-        public static float timecode;
-        public static GameController Instance;
-        public Root ChartData;
-        public List<LineRenderer> GuideLines = new List<LineRenderer>();
-        public List<Tap> Taps = new List<Tap>();
-        public List<Drag> Drags = new List<Drag>();
-        public List<Flick> Flicks = new List<Flick>();
-        private int frames = 0;
+		// 静态时间码和实例
+		public static float timecode;
+		public static GameController Instance;
+		public Root ChartData;
+		public List<LineRenderer> GuideLines = new List<LineRenderer>();
+		public List<Tap> Taps = new List<Tap>();
+		public List<Drag> Drags = new List<Drag>();
+		public List<Flick> Flicks = new List<Flick>();
+		private int frames = 0;
 
-        // 构造函数，实例化静态实例
-        public GameController()
-        {
-            Instance = this;
-        }
+		// 构造函数，实例化静态实例
+		public GameController()
+		{
+			Instance = this;
+		}
 
-        // 点的列表
-        private List<List<Vector2>> point = new List<List<Vector2>>
-        {
-            new List<Vector2>
-                { new Vector2(0, 0), new Vector2(100, -1000), new Vector2(1000, -10), new Vector2(1000, -1000) }
-        };
+		// 点的列表
+		private List<List<Vector2>> point = new List<List<Vector2>>
+		{
+			new List<Vector2>
+				{ new Vector2(0, 0), new Vector2(100, -1000), new Vector2(1000, -10), new Vector2(1000, -1000) }
+		};
 
-        // _Ready方法，当节点准备好时调用
-        public override void _Ready()
-        {
-            if (isAutoPlay == null)
-                isAutoPlay = true;
+		// _Ready方法，当节点准备好时调用
+		public override void _Ready()
+		{
+			if (isAutoPlay == null)
+				isAutoPlay = true;
 
-            // 设置最大帧率
-            Engine.MaxFps = 60;
-            string chartPath = "res://Chart/TestJson.json";
-            string jsonString = FileAccess.GetFileAsString(chartPath);
-            ChartData = ChartAnalyser.GetChartData(jsonString);
-            //设置标题和难度
-            NameLabel.Text = ChartData.Name + " " + ChartData.Hard;
-            ChartPlay();
-            //Debug Code
-            // 创建新的线渲染器并添加到节点
-            // LineRenderer line = LineRenderer.newLineRenderer<LineRenderer>(lineRenderer);
-            // line.SetLineRenderer(point);
-            // AddChild(line);
-            // // 创建新的Tap并添加到节点
-            // Tap _tap = Tap.newNote<Tap>(tap);
-            // _tap.Visible = true; //设置可见性
-            // AddChild(_tap);
-            // _tap.InitNote(line, [1, 0, 4]);
-            //
-            // _tap.Connect("DestroyTap", new Callable(this, nameof(OnDestroyTap))); //连接摧毁Tap信号
-            //
-            //
-            // // 创建新的Drag并添加到节点
-            // Drag _drag = Drag.newNote<Drag>(drag);
-            // _drag.Visible = true; //设置可见性
-            // AddChild(_drag);
-            // _drag.InitNote(line, [1, 1, 4]);
-            //
-            // _drag.Connect("DestroyDrag", new Callable(this, nameof(OnDestroyDrag))); //连接摧毁Drag信号
-            //
-            //
-            // // 创建新的Flick并添加到节点
-            // Flick _flick = Flick.newNote<Flick>(flick);
-            // _flick.Visible = true; //设置可见性
-            // AddChild(_flick);
-            // _flick.InitNote(line, [1, 2, 4]);
-            //
-            // _flick.Connect("DestroyFlick", new Callable(this, nameof(OnDestroyFlick))); //连接摧毁Flick信号
-        }
+			// 设置最大帧率
+			Engine.MaxFps = 60;
+			string chartPath = "res://Chart/TestJson.json";
+			string jsonString = FileAccess.GetFileAsString(chartPath);
+			ChartData = ChartAnalyser.GetChartData(jsonString);
+			//设置标题和难度
+			NameLabel.Text = ChartData.Name + " " + ChartData.Hard;
+			ChartPlay();
+			//Debug Code
+			// 创建新的线渲染器并添加到节点
+			// LineRenderer line = LineRenderer.newLineRenderer<LineRenderer>(lineRenderer);
+			// line.SetLineRenderer(point);
+			// AddChild(line);
+			// // 创建新的Tap并添加到节点
+			// Tap _tap = Tap.newNote<Tap>(tap);
+			// _tap.Visible = true; //设置可见性
+			// AddChild(_tap);
+			// _tap.InitNote(line, [1, 0, 4]);
+			//
+			// _tap.Connect("DestroyTap", new Callable(this, nameof(OnDestroyTap))); //连接摧毁Tap信号
+			//
+			//
+			// // 创建新的Drag并添加到节点
+			// Drag _drag = Drag.newNote<Drag>(drag);
+			// _drag.Visible = true; //设置可见性
+			// AddChild(_drag);
+			// _drag.InitNote(line, [1, 1, 4]);
+			//
+			// _drag.Connect("DestroyDrag", new Callable(this, nameof(OnDestroyDrag))); //连接摧毁Drag信号
+			//
+			//
+			// // 创建新的Flick并添加到节点
+			// Flick _flick = Flick.newNote<Flick>(flick);
+			// _flick.Visible = true; //设置可见性
+			// AddChild(_flick);
+			// _flick.InitNote(line, [1, 2, 4]);
+			//
+			// _flick.Connect("DestroyFlick", new Callable(this, nameof(OnDestroyFlick))); //连接摧毁Flick信号
+		}
 
-        public void ChartPlay()
-        {
-            foreach (ChartItem Coordinate in ChartData.Chart)
-            {
-                foreach (LinesItem lineGroup in Coordinate.coordinate.lines)
-                {
-                    List<List<Vector2>> point = new List<List<Vector2>>();
-                    
-                    for (int i = 0; i < lineGroup.line.Count; i++)
-                    {
-                        // 因为Godot使用的坐标系和一般的坐标系相反，所以需要反向坐标轴
-                        point = new List<List<Vector2>>
-                        {
-                            new List<Vector2>
-                            {
-                                new(lineGroup.line[i].start[0], -lineGroup.line[i].start[1]),
-                                new(lineGroup.line[i].p1[0], -lineGroup.line[i].p1[1]),
-                                new(lineGroup.line[i].p2[0], -lineGroup.line[i].p2[1]),
-                                new(lineGroup.line[i].end[0], -lineGroup.line[i].end[1])
-                            }
-                        };
-                        
-                    }
-                    //TODO:使用两段式绘制线段后，有Bug，线不显示
-                    GuideLines.Add(LineRenderer.newLineRenderer<LineRenderer>(this.lineRenderer)); // 创建新的线渲染器并添加到节点
-                    GuideLines[lineGroup.num].SetLineRenderer(point);
-                    AddChild(GuideLines[lineGroup.num]);
-                    foreach (NoteItem note in lineGroup.note)
-                    {
-                        GD.Print($"{note.time}");
-                    }
-                }
-            }
-        }
+		public void ChartPlay()
+		{
+			foreach (ChartItem Coordinate in ChartData.Chart)
+			{
+				foreach (LinesItem lineGroup in Coordinate.coordinate.lines)
+				{
+					List<List<Vector2>> point = new List<List<Vector2>>();
+					
+					for (int i = 0; i < lineGroup.line.Count; i++)
+					{
+						// 因为Godot使用的坐标系和一般的坐标系相反，所以需要反向坐标轴
+						point = new List<List<Vector2>>
+						{
+							new List<Vector2>
+							{
+								new(lineGroup.line[i].start[0], -lineGroup.line[i].start[1]),
+								new(lineGroup.line[i].p1[0], -lineGroup.line[i].p1[1]),
+								new(lineGroup.line[i].p2[0], -lineGroup.line[i].p2[1]),
+								new(lineGroup.line[i].end[0], -lineGroup.line[i].end[1])
+							}
+						};
+						
+					}
+					//TODO:使用两段式绘制线段后，有Bug，线不显示
+					GuideLines.Add(LineRenderer.newLineRenderer<LineRenderer>(this.lineRenderer)); // 创建新的线渲染器并添加到节点
+					GuideLines[lineGroup.num].SetLineRenderer(point);
+					AddChild(GuideLines[lineGroup.num]);
+					foreach (NoteItem note in lineGroup.note)
+					{
+						GD.Print($"{note.time}");
+					}
+				}
+			}
+		}
 
-        // 创建Note
-        public void CreateNote()
-        {
-            
-        }
-        
-        /// <summary>
-        /// 用于信号连接的函数
-        /// 主要用来处理打击特效和判定
-        /// </summary>
-        // 处理销毁Tap信号
-        public void OnDestroyTap()
-        {
-            GD.Print($"Connected");
-        }
+		// 创建Note
+		public void CreateNote()
+		{
+			
+		}
+		
+		/// <summary>
+		/// 用于信号连接的函数
+		/// 主要用来处理打击特效和判定
+		/// </summary>
+		// 处理销毁Tap信号
+		public void OnDestroyTap()
+		{
+			GD.Print($"Connected");
+		}
 
-        // 处理销毁Drag信号
-        public void OnDestroyDrag()
-        {
-            GD.Print($"Connected");
-        }
+		// 处理销毁Drag信号
+		public void OnDestroyDrag()
+		{
+			GD.Print($"Connected");
+		}
 
-        // 处理销毁Flick信号
-        public void OnDestroyFlick()
-        {
-            GD.Print($"Connected");
-        }
+		// 处理销毁Flick信号
+		public void OnDestroyFlick()
+		{
+			GD.Print($"Connected");
+		}
 
-        // 设置时间码
-        private void setTimeCode(int f)
-        {
-            timecode = frames / Engine.MaxFps;
-        }
+		// 设置时间码
+		private void setTimeCode(int f)
+		{
+			timecode = frames / Engine.MaxFps;
+		}
 
-        // 处理每帧的逻辑更新
-        public override void _Process(double delta)
-        {
-            frames++;
+		// 处理每帧的逻辑更新
+		public override void _Process(double delta)
+		{
+			frames++;
 
 
-            // tap.update();
-        }
+			// tap.update();
+		}
 
-        // 物理逻辑更新
-        public override void _PhysicsProcess(double delta)
-        {
-        }
-    }
+		// 物理逻辑更新
+		public override void _PhysicsProcess(double delta)
+		{
+		}
+	}
 }
--- Script/UI/ChartEditor/CreateChartButtonActions.gd
+++ Script/UI/ChartEditor/CreateChartButtonActions.gd
@@ -5,9 +5,14 @@ extends Control
 @export var MusicStream:AudioStream
 @export var MusicPlayer:AudioStreamPlayer2D
 
+@export var _MusicName:LineEdit
+@export var _ChartName:LineEdit
+@export var _ArtistName:LineEdit
+@export var _DefaultBPM:LineEdit
 
 var _Path
 
+var _SystemPath
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,38 +29,49 @@ func _open_music_file():
 	file_dialog.set_access(FileDialog.ACCESS_FILESYSTEM)
 	file_dialog.set_title("Open Music File")
 	file_dialog.add_filter("*.mp3")
+#	file_dialog.add_filter("*.wav")——见下
 	print(OS.get_environment("USER"))
 	# 
-	if OS.get_name()=="Window":
+	if OS.get_name()=="Windows":
 		if OS.has_environment("USERNAME"):
 			var username = OS.get_environment("USERNAME")
-			file_dialog.set_current_dir("C:/Users/"+username+"/Music")
+			_SystemPath = "C:\\Users\\"+username+"\\"
+			file_dialog.set_current_dir(_SystemPath+"Music")
 			# 如果'USERNAME'不存在，尝试'USER'环境变量（通常在Linux和macOS上使用）
 		elif OS.has_environment("USER"):
 			var username = OS.get_environment("USER")
-			file_dialog.set_current_dir("C:/Users/"+username+"/Music")
+			_SystemPath = "C:\\Users\\"+username+"\\"
+			file_dialog.set_current_dir(_SystemPath+"Music")
 	elif OS.get_name()=="macOS":
 		if OS.has_environment("USERNAME"):
 			var username = OS.get_environment("USERNAME")
-			file_dialog.set_current_dir("/Users/"+username+"/Music")
+			_SystemPath = "/Users/"+username+"/"
+			file_dialog.set_current_dir(_SystemPath+"Music")
 			# 如果'USERNAME'不存在，尝试'USER'环境变量（通常在Linux和macOS上使用）
 		elif OS.has_environment("USER"):
 			var username = OS.get_environment("USER")
-			file_dialog.set_current_dir("/Users/"+username+"/Music")
+			_SystemPath = "/Users/"+username+"/"
+			file_dialog.set_current_dir(_SystemPath+"Music")
 		
 	
 	file_dialog.connect("file_selected", Callable(self, "_music_file_selected"))
 	add_child(file_dialog)
 	file_dialog.popup_centered(Vector2(1000,900))
-	
 
 
 func _music_file_selected(path):
 	PathLabel.text = path
 	print("Selected music file: " + path)
+	var file_type_chars = path.right(4)
 	#播放音频
 	var MusicFile = FileAccess.open(path,FileAccess.READ)
-	var stream = AudioStreamMP3.new()
+	var stream
+	if(file_type_chars==".mp3"):
+		stream = AudioStreamMP3.new()
+#	会将wav文件头读取为音频文件，因此需要修改
+#	elif(file_type_chars==".wav"):
+#		stream = AudioStreamWAV.new()
+	print(file_type_chars)
 	stream.data = MusicFile.get_buffer(MusicFile.get_length())
 	MusicPlayer.stream = stream
 	MusicPlayer.play()
@@ -64,4 +80,10 @@ func _music_file_selected(path):
 
 
 func _on_create_button_down():
-	pass # Replace with function body.
+#	var yaml = preload("res://godot-yaml-Windows_x64/gdyaml.gdns").new() TODO:如何用插件转换为YAML
+	var File = FileAccess.open("D:\\GodotTester\\CodeWTF.txt",FileAccess.WRITE)
+	File.store_string(_MusicName.text+'\n')
+	File.store_string(_ChartName.text+'\n')
+	File.store_string(_ArtistName.text+'\n')
+	File.store_string(_DefaultBPM.text+'\n')
+	File.close()
